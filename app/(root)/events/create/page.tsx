ae0137fc9aefdc884d3c82261c6b9977587b0f05
Shailesh Darji <shaileshmahesh190@gmail.com>
2024-03-28 23:43:22 +0530

    Update page.tsx

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,11 +1,12 @@
 import { EventForm } from "@/components/shared/event-form";
-import { auth } from "@clerk/nextjs";
+import { auth,currentUser } from "@clerk/nextjs";
 
 const CreateEvent = () => {
-  const { sessionClaims } = auth();
-
-  const userId = sessionClaims?.userId as string;
-
+  // const { sessionClaims } = auth();
+  
+  // const userId = sessionClaims?.userId as string;
+  const user =  await currentUser();
+  const userId = user?.id as string;
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
